86f00e2fdf535f146d1915bc1f6ad7fe286b24ed
Bernabe Daniel Inche Ticlavilca <85372343+DAMN1SELF@users.noreply.github.com>
2025-09-05 15:58:42 -0500

    POST AgregarProducto

---
+++ Carrito/Application/UseCases/AddItemToCart.cs
@@ -0,0 +1,44 @@
+﻿using INCHE.Carrito_Compras.Application.Ports;
+using INCHE.Carrito_Compras.Domain.Carrito;
+using INCHE.Carrito_Compras.Domain.Common;
+using INCHE.Carrito_Compras.Dtos.Requests;
+
+namespace INCHE.Carrito_Compras.Application.UseCases
+{
+    public class AddItemToCart
+    {
+        private readonly ICartRepository _repo;
+        private readonly IProductRuleProvider _rules;
+
+        public AddItemToCart(ICartRepository repo, IProductRuleProvider rules)
+        {
+            _repo = repo;
+            _rules = rules;
+        }
+
+        public Resultado<Carrito> Execute(string cartId, AddToCartRequest req)
+        {
+            var prules = _rules.Get(req.ProductId) ?? _rules.GetDefault();
+            if (req.ProductId != prules.ProductId)
+                return Resultado<Carrito_Compras.Domain.Carrito.Carrito>.Fail("ProductId inválido para las reglas.");
+
+            var cart = _repo.Get(cartId) ?? Carrito.Crear(cartId);
+
+            var selecciones = new List<SeleccionGrupo>();
+            foreach (var s in req.Selections)
+            {
+                var attrs = new List<SeleccionAtributo>();
+                foreach (var a in s.Attributes)
+                    attrs.Add(new SeleccionAtributo(a.AttributeId, a.Quantity));
+
+                selecciones.Add(new SeleccionGrupo(s.GroupAttributeId, attrs));
+            }
+
+            var r = cart.Agregar(prules.ProductId, req.Quantity, prules.BasePrice, selecciones);
+            if (!r.EsExitoso) return Resultado<Carrito_Compras.Domain.Carrito.Carrito>.Fail(r.Error!);
+
+            _repo.Upsert(cart);
+            return Resultado<Carrito_Compras.Domain.Carrito.Carrito>.Ok(cart);
+        }
+    }
+}
---
+++ Carrito/Controllers/CartsController.cs
@@ -0,0 +1,41 @@
+﻿using INCHE.Carrito_Compras.Application.Mappers;
+using INCHE.Carrito_Compras.Application.UseCases;
+using INCHE.Carrito_Compras.Dtos.Requests;
+using INCHE.Carrito_Compras.Dtos.Responses;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarritoCompras.Controllers
+{
+    public class CartsController : Controller
+    {
+        private readonly AddItemToCart _addItem; 
+        private readonly GetCart _getCart;
+        private readonly ICartMapper _mapper;
+        public CartsController(
+         AddItemToCart addItem, GetCart getCart,
+            ICartMapper mapper)
+        {
+            _addItem = addItem; 
+            _mapper = mapper; 
+            _getCart = getCart;
+
+        }
+
+        [HttpPost("producto")]
+        public ActionResult<CartResponse> AddProducto(string cartId, [FromBody] AddToCartRequest body)
+        {
+        
+            var res = _addItem.Execute(cartId, body) ;
+            if (!res.EsExitoso) return UnprocessableEntity(new { error = res.Error });
+
+            var (cart, _) = _getCart.Execute(cartId) ;   
+            return Ok(_mapper.ToResponse(cart!));       
+        }
+
+
+  
+
+
+    }
+}
